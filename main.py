b33ed1aaab8e7c4ff03a50bf2458dd488fa9d7db
Chirag Gomber <36930204+ArtistBanda@users.noreply.github.com>
2019-01-21 01:21:32 +0530

    Create main.py
    Python homework #1

---
+++ main.py
@@ -0,0 +1,18 @@
+# Declaring the details for
+# the specified song
+Genre = 'Pop'
+Title = 'Sunflower'
+Artist = 'Post Malone and Swae Lee'
+ReleaseDate = '18/10/2018'
+DurationInSeconds = 159
+DurationInMinutes = 2.65
+
+
+# Printing out the details for
+# the specified song
+print("Genre : ", Genre)
+print("Title : ", Title)
+print("Artist : ", Artist)
+print("Release Date : ", ReleaseDate)
+print("Duration in Seconds : ", DurationInSeconds)
+print("Duration in Minutes : ", DurationInMinutes)
